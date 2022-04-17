35f60b252092990cb9fc0ae455898eb5afd3d815
Shakedgo <shakedgo@gmail.com>
2022-04-17 22:05:06 +0300

    Added click visualizations

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function build(newTable){
+function build(newTable) {
     for (let i = 0; i < 8; i++) {
         let tr = document.createElement('tr');
         newTable.appendChild(tr); // creates new tr
         for (let j = 0; j < 8; j++) {
             // i and j to switch places
             const td = document.createElement('td');
-            td.style='text-align: center; font-size: 60px;';
+            td.style = 'text-align: center; font-size: 60px;';
             if ((j + i) % 2 === 0) {
                 td.className = 'white';
                 tr.appendChild(td);
@@ -14,54 +14,60 @@ function build(newTable){
                 td.className = 'dark';
                 tr.appendChild(td);
             }
-            let count = 0;
-            td.addEventListener("click", () => {td.classList.toggle("clicked")})
+            if (i === 0) {
+                if (j===0 || j === 7) {
+                    addImg(td,'BlackRook');
+                } else if (j === 1 || j===6) {
+                    addImg(td, 'BlackKnight');
+                } else if (j === 2 || j===5) {
+                    addImg(td, 'BlackBishop');
+                } else if (j===3) {
+                    addImg(td, 'BlackQueen');
+                } else if (j===4) {
+                    addImg(td, 'BlackKing');
+                }
+            } else if (i === 1) {
+                addImg(td, 'BlackPawn');
+            } else if (i === 6) {
+                addImg(td, 'WhitePawn');
+            } else if (i === 7) {
+                if (j===0 || j === 7) {
+                    addImg(td,'WhiteRook');
+                } else if (j === 1 || j===6) {
+                    addImg(td, 'WhiteKnight');
+                } else if (j === 2 || j===5) {
+                    addImg(td, 'WhiteBishop');
+                } else if (j===3) {
+                    addImg(td, 'WhiteQueen');
+                } else if (j===4) {
+                    addImg(td, 'WhiteKing');
+                }
+            }
+            td.onclick = () => { cellClick(td) };
         }
     }
 }
 
-function populate(newTable) {
-    getCell = (x,y) =>{
-        return newTable.rows[x].cells[y];
-    }
-    addPieces = (path) => {
-        let img = document.createElement('img');
-        img.style = 'width: 60px;height:60px;'
-        img.src ='pieces/' + path + '.svg';
-        return img;
-    }
-    getCell(0,0).appendChild(addPieces('BlackRook'))
-    getCell(0,7).appendChild(addPieces('BlackRook'))
-    getCell(7,0).appendChild(addPieces('WhiteRook'))
-    getCell(7,7).appendChild(addPieces('WhiteRook'))
-    
-    getCell(0,1).appendChild(addPieces('BlackKnight'))
-    getCell(0,6).appendChild(addPieces('BlackKnight'))
-    getCell(7,1).appendChild(addPieces('WhiteKnight'))
-    getCell(7,6).appendChild(addPieces('WhiteKnight'))
-    
-    getCell(0,2).appendChild(addPieces('BlackBishop'))
-    getCell(0,5).appendChild(addPieces('BlackBishop'))
-    getCell(7,2).appendChild(addPieces('WhiteBishop'))
-    getCell(7,5).appendChild(addPieces('WhiteBishop'))
+function addImg(cell, name) {
+    const img = document.createElement('img');
+    img.style = 'width: 60px;height:60px;'
+    img.src = 'pieces/' + name + '.svg';
+    cell.appendChild(img);
+    console.log(img.src)
+}
 
-    getCell(0,3).appendChild(addPieces('BlackQueen'))
-    getCell(7,3).appendChild(addPieces('WhiteQueen'))
-
-    getCell(0,4).appendChild(addPieces('BlackKing'))
-    getCell(7,4).appendChild(addPieces('WhiteKing'))
-
-    for (let i=0; i<8; i++){
-        getCell(1,i).appendChild(addPieces('BlackPawn'))
+function cellClick(td) {
+    selectedCell = document.querySelector(".clicked")
+    console.log("td = " + td)
+    console.log("selectedCell = " + selectedCell)
+    if (selectedCell !== null) {
+        selectedCell.classList.remove("clicked")
     }
-    for (let i=0; i<8; i++){
-        getCell(6,i).appendChild(addPieces('WhitePawn'))
-    }
+    td.classList.toggle("clicked")
+    selectedCell = td
 
 }
 
 const newTable = document.createElement('table');
-document.body.appendChild(newTable); // Create table;
-
-build(newTable)
-populate(newTable);
+document.body.appendChild(newTable); // Create table.
+build(newTable) // Builds the chessboard.
