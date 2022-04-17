09bcac20ea7d0f7fac5c334b002c51fda00d4292
Shakedgo <shakedgo@gmail.com>
2022-04-17 14:32:02 +0300

    Changed the build of the chessboard and pieces

--- script.js
+++ script.js
@@ -1,58 +1,67 @@
+function build(newTable){
+    for (let i = 0; i < 8; i++) {
+        let tr = document.createElement('tr');
+        newTable.appendChild(tr); // creates new tr
+        for (let j = 0; j < 8; j++) {
+            // i and j to switch places
+            const td = document.createElement('td');
+            td.style='text-align: center; font-size: 60px;';
+            if ((j + i) % 2 === 0) {
+                td.className = 'white';
+                tr.appendChild(td);
+            }
+            else {
+                td.className = 'dark';
+                tr.appendChild(td);
+            }
+            ////newTable.addEventListener('click', () => {th.className = 'clicked';})
+        }
+    }
+}
+
+function populate(newTable) {
+    getCell = (x,y) =>{
+        return newTable.rows[x].cells[y];
+    }
+    addPieces = (path) => {
+        let img = document.createElement('img');
+        img.style = 'width: 60px;height:60px;'
+        img.src ='pieces/' + path + '.svg';
+        return img;
+    }
+    getCell(0,0).appendChild(addPieces('BlackRook'))
+    getCell(0,7).appendChild(addPieces('BlackRook'))
+    getCell(7,0).appendChild(addPieces('WhiteRook'))
+    getCell(7,7).appendChild(addPieces('WhiteRook'))
+    
+    getCell(0,1).appendChild(addPieces('BlackKnight'))
+    getCell(0,6).appendChild(addPieces('BlackKnight'))
+    getCell(7,1).appendChild(addPieces('WhiteKnight'))
+    getCell(7,6).appendChild(addPieces('WhiteKnight'))
+    
+    getCell(0,2).appendChild(addPieces('BlackBishop'))
+    getCell(0,5).appendChild(addPieces('BlackBishop'))
+    getCell(7,2).appendChild(addPieces('WhiteBishop'))
+    getCell(7,5).appendChild(addPieces('WhiteBishop'))
+
+    getCell(0,3).appendChild(addPieces('BlackQueen'))
+    getCell(7,3).appendChild(addPieces('WhiteQueen'))
+
+    getCell(0,4).appendChild(addPieces('BlackKing'))
+    getCell(7,4).appendChild(addPieces('WhiteKing'))
+
+    for (let i=0; i<8; i++){
+        getCell(1,i).appendChild(addPieces('BlackPawn'))
+    }
+    for (let i=0; i<8; i++){
+        getCell(6,i).appendChild(addPieces('WhitePawn'))
+    }
+
+}
+
+
 const newTable = document.createElement('table');
 document.body.appendChild(newTable); // Create table;
 
-for (let i = 0; i < 8; i++) {
-    let tr = document.createElement('tr');
-    newTable.appendChild(tr); // creates new tr
-    for (let j = 0; j < 8; j++) {
-        // i and j to switch places
-        const td = document.createElement('td');
-        td.style='text-align: center; font-size: 60px;';
-        if ((j + i) % 2 === 0) {
-            td.className = 'white';
-            tr.appendChild(td);
-        }
-        else {
-            td.className = 'dark';
-            tr.appendChild(td);
-        }
-        // Fuck this is awful.
-        if (i === 0 && j===0 || i === 0 && j ===7){
-            td.innerHTML = '&#9820;'; //Black Rook
-        }
-        if (i === 7 && j===7 || i === 7 && j ===0){
-            td.innerHTML = '&#9814;'; //White Rook
-        }
-        if (i === 0 && j === 1 || i===0 && j===6){
-            td.innerHTML = '&#9822;'; //Black Knight
-        }
-        if (i === 7 && j === 6 || i===7 && j===1){
-            td.innerHTML = '&#9816;'; //White Knight
-        }
-        if (i === 0 && j === 2 || i===0 && j===5){
-            td.innerHTML = '&#9821;'; //Black Bishop
-        }
-        if (i === 7 && j === 5 || i===7 && j===2){
-            td.innerHTML = '&#9815;'; //White Bishop
-        }
-        if (i === 1){
-            td.innerHTML = '&#9823;'; //Black Pawn
-        }
-        if (i === 6){
-            td.innerHTML = '&#9817;'; //White Pawn
-        }
-        if (i === 0 && j === 3) {
-            td.innerHTML = '&#9819;'; //Black Queen
-        }
-        if (i === 7 && j === 3) {
-            td.innerHTML = '&#9813;'; //White Queen
-        }
-        if (i === 0 && j === 4) {
-            td.innerHTML = '&#9818;'; //Black King
-        }
-        if (i === 7 && j === 4) {
-            td.innerHTML = '&#9812;'; //White King
-        }
-        //newTable.addEventListener('click', () => {th.className = 'clicked';})
-    }
-}
+build(newTable)
+populate(newTable);
